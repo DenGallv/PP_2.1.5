911dc9a49fab83d2af6b2d21df3fc0416bce3329
Denis Gallyamov <gallyamovda@yandex.ru>
2023-05-12 13:43:27 +0300

    init commit

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,13 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    private String end = "Конец";
+
+    @Override
+    public String toString() {
+        return end;
+    }
 }
